cf8962980592fc0428f801e696e289e50b1db7e4
dixon <dixon9197@gmail.com>
2016-07-05 11:53:40 +0800

    add nslog in app delegate file

--- ShellApp/ViewController.swift
+++ ShellApp/ViewController.swift
@@ -26,8 +26,6 @@ import JavaScriptCore
 
     @IBOutlet var wv:UIWebView!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
